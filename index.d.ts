e274f97cae2860168019c7f498d1c87e0b69ca3d
Tuğrul Topuz <tugrultopuz@gmail.com>
2020-12-02 00:59:45 +0300

    ts mapping correction

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,17 @@
 
 import {Transform} from 'stream';
 
-interface CsvFilterOptions {
-    delimiter?: string,
-    newLine?: string,
-    targetDelimiter?: string,
-    targetNewLine?: string
-    skipFirstLine?: boolean,
-    filter?: (cols: Buffer[], chunk: Buffer) => Buffer[]
+export default class CsvFilter extends Transform {
+    constructor(opts?: CsvFilter.CsvFilterOptions);
 }
 
-export default class CsvFilter extends Transform {
-    constructor(opts?: CsvFilterOptions);
+declare namespace CsvFilter {
+    export interface CsvFilterOptions {
+        delimiter?: string,
+        newLine?: string,
+        targetDelimiter?: string,
+        targetNewLine?: string
+        skipFirstLine?: boolean,
+        filter?: (cols: Buffer[], chunk: Buffer) => Buffer[]
+    }
 }
